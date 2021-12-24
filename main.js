e2c300ec8fae8d082f33ea122906ca0df6706509
Rayudo11 <bryanmendez209@gmail.com>
2021-12-24 15:56:57 -0600

    initial commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 // brings in the assert module for unit testing
 const assert = require('assert');
+const { stringify } = require('querystring');
 // brings in the readline module to access the command line
 const readline = require('readline');
 // use the readline module to print out to the command line
@@ -10,12 +11,37 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// We must indicate that we are looking for vowels in our input//
+const vowels = ['a','e','i','o','u'];
+let newWord = '';
+let ending = 'yay';
+let ending2 = 'ay';
+//We create the part of the function for when the first letter in the string is a vowel//
+const pigLatin = (str) => {
+  if (vowels.includes(str[0])){
+  let lowerC = newWord.toLowerCase();
+  return  lowerC + ending
+  }
+  // now we create the part of the function where if the word starts with a consonant; save the consonants and find the first vowel//
+  else{
+   let consonants='';
+   for(let i = 0; i < str.length; i++){
+     if (vowels.includes(str[i])){
+      break;
+      }
+     consonants += str[i];
+    }
+   return str.substring(consonants.length) + consonants + ending2;
+  }
 
-const pigLatin = (word) => {
+}
+
+pigLatin('Climb')
+
+
 
   // Your code here
 
-}
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
